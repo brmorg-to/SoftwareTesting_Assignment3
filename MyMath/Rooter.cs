d6f59f4a80c3338b37d27d3a71dd2c5cbc1fef87
Bruno Morgado <brunomorgado@Brunos-iMac.local>
2022-05-26 16:51:17 -0400

    first commit

---
+++ MyMath/Rooter.cs
@@ -0,0 +1,36 @@
+﻿namespace UnitTestProject1;
+
+public class Rooter
+{
+    public double SquareRoot(double input)
+    {
+        //throw new NotImplementedException();
+
+        if (input <= 0.0)
+
+        {
+
+            throw new ArgumentOutOfRangeException();
+
+        }
+
+        double result = input;
+
+        double previousResult = -input;
+
+        while (Math.Abs(previousResult - result) > result / 1000)
+
+        {
+
+            previousResult = result;
+
+            result = result - (result * result - input) / (2 * result);
+
+        }
+
+        return result;
+
+
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,99 @@
+﻿using static System.Console;
+using NUnit.Framework;
+
+namespace UnitTestProject1;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void BasicRooterTest()
+
+    {
+
+        //	Create an instance to test:
+        Rooter rooter = new Rooter();
+
+        //	Define a test input and output value:
+        double expectedResult = 2.0;
+
+        double input = expectedResult * expectedResult;
+
+        //	Run the method under test:
+
+        double actualResult = rooter.SquareRoot(input);
+
+        //	Verify the result:
+        Assert.That(actualResult, Is.EqualTo(expectedResult).Within(expectedResult / 100));
+
+    }
+
+
+    [Test]
+    public void RooterValueRange()
+    {
+        //	Create an instance to test:
+        Rooter rooter = new Rooter();
+
+        //	Try a range of values:
+        for (double expectedResult = 1e-8;
+
+        expectedResult < 1e+8;
+
+        expectedResult = expectedResult * 3.2)
+
+        {
+
+            RooterOneValue(rooter, expectedResult);
+
+        }
+
+    }
+
+    private void RooterOneValue(Rooter rooter, double expectedResult)
+    {
+
+        double input = expectedResult * expectedResult;
+
+        double actualResult = rooter.SquareRoot(input);
+
+        Assert.That(actualResult, Is.EqualTo(expectedResult).Within(expectedResult / 1000));
+
+    }
+
+
+    [Test]
+
+    public void RooterTestNegativeInputx()
+
+    {
+
+        Rooter rooter = new Rooter();
+
+        try
+
+        {
+
+            rooter.SquareRoot(-10);
+
+        }
+
+        catch (ArgumentOutOfRangeException)
+
+        {
+
+            return;
+
+        }
+
+        Assert.Fail();
+
+    }
+
+
+
+}
